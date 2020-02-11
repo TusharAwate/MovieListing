c9fe2fbdf22422621b2fd8b564cfd12948dc5991
Supriya Saxena <supriyasaxena@Supriyas-MacBook-Pro.local>
2020-02-11 22:40:36 +0000

    Add .gitignore Add unit tests with snapshots

--- src/components/Movies/tests/Movie.test.js
+++ src/components/Movies/tests/Movie.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { render } from '@testing-library/react';
+import Movie from '../Movie';
+describe('Movie', () => {
+    it('should render and match the snapshot', () => {
+        const props = {
+            movie: { poster_path: 'test_poster_path', title: 'test_title' },
+            config: { images: { secure_base_url: 'test_secure_base_url' } },
+            genres: ['genre1', 'genre2']
+        };
 
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-    ReactDOM.unmountComponentAtNode(div);
+
+        const {
+            container: { firstChild },
+        } = render(<Movie {...props} />);
+
+        expect(firstChild).toMatchSnapshot();
+    });
+
 });
+
